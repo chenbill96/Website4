c46f6d49e5bedca282ec8ba06f06848de0340568
Bill Chen <billchen@MBP-BCHEN.local>
2015-08-24 23:52:53 -0400

    added about page

--- js/global.js
+++ js/global.js
@@ -1,11 +1,14 @@
-$(document).ready(function () {
+$(document).ready(function(){
+
 	$('span.smallNavButton').click(function () {
 		$('#navItems').slideToggle();
+		console.log('works');
 	});
 
 	$(window).resize(function (){
 		if( $(window).width() > 890) {
 			$('#navItems').removeAttr('style');
 		}
-	})
+	});
+
 });
